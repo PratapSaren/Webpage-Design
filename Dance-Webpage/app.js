c4fd953d52aab9f9f91cbd48c542f7401bb0f21d
PRATAP SAREN <axelblaze.pratap.saren@gmail.com>
2020-11-19 22:47:27 +0530

    Changes in Backend Part

--- Dance-Webpage/app.js
+++ Dance-Webpage/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactDance', {useNewUrlParser: true});
 const app = express();
 const port = 8000;
+
+//Define schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+  });
+
+var contact = mongoose.model('contact', contactSchema);
+
 
 app.use('/static',express.static('static'));
 app.use(express.urlencoded());
@@ -21,6 +36,19 @@ app.get('/contact',(req,res)=>{
     const params = {};
     res.status(200).render('contact.pug', params);
 })
+
+app.post('/contact',(req,res)=>{
+    var myData = new contact(req.body)
+    myData.save().then(()=>{
+        res.send("This data has been saved to the Database")
+    }).catch(()=>{
+        res.status(404).send("Item was not saved to the database")
+    });
+    //res.status(200).render('contact.pug', params);
+})
+
+
+
 
 app.listen(port,()=>{
     console.log(`The application started successfully on port ${port}`);
